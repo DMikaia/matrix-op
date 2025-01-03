6057b427994404f613f107db94eae2a03ff48e60
DMikaia <faeldan03@gmail.com>
2025-01-03 18:09:19 +0300

    config: added both example of add and multiply in the program

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use matrix_op::{display_matrix, Matrix};
 
 fn main() {
-    let matrix_a: Matrix<f64> = Matrix::create_with(3, 2, vec![3.0, 4.0, 7.0, 2.0, 5.0, 9.0]);
-    let matrix_b: Matrix<f64> = Matrix::create_with(2, 3, vec![3.0, 1.0, 5.0, 6.0, 9.0, 7.0]);
+    let matrix_a: Matrix<f64> =
+        Matrix::create_with(3, 3, vec![3.0, 4.0, 7.0, 2.0, 5.0, 9.0, 2.0, 5.0, 9.0]);
+    let matrix_b: Matrix<f64> =
+        Matrix::create_with(3, 3, vec![3.0, 1.0, 5.0, 6.0, 9.0, 7.0, 2.0, 5.0, 9.0]);
 
     display_matrix(&matrix_a);
 
